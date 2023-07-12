db9fadbbaf12637d810e14113cd81328c9b7e6b7
Serhii-Pashchenko <paschenko09@gmail.com>
2023-07-12 21:35:24 +0300

    .

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
 
 app.get('/', (req, res) => {
-  res.send(`Server is running on port ${PORT}`);
+  res.send(`${process.env.DB_URL}`);
 });
 
 app.use('/api', userRoutes);
